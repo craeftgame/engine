614fc9a8519c8c391ad3ab603bd61910c6d74d5d
Sebastian <13647606+H34D@users.noreply.github.com>
2026-01-19 10:04:17 +0100

    fix: harden tests so results are more predictable
    Signed-off-by: Sebastian <13647606+H34D@users.noreply.github.com>

--- src/game/items/__tests__/armor.test.ts
+++ src/game/items/__tests__/armor.test.ts
@@ -1,13 +1,15 @@
 import assert from "assert";
 import { Armor } from "../";
 import Craeft from "../../../craeft";
+import { Rarities } from "../../../data";
 
 describe("Armor", () => {
   const craeft = new Craeft();
   describe("constructor", () => {
     it("should set def correct", () => {
       const def = 10;
-      const item = new Armor({ craeft, def });
+      const item = new Armor({ craeft, def, rarity: Rarities.Common });
+
       assert.equal(item.def(), def);
     });
   });
--- src/game/items/__tests__/weapon.test.ts
+++ src/game/items/__tests__/weapon.test.ts
@@ -1,13 +1,14 @@
 import assert from "assert";
 import { Weapon } from "../";
 import Craeft from "../../../craeft";
+import { Rarities } from "../../../data";
 
 describe("Weapon", () => {
   const craeft = new Craeft();
   describe("constructor", () => {
     it("should set atk correct", () => {
       const atk = 10;
-      const item = new Weapon({ craeft, atk });
+      const item = new Weapon({ craeft, atk, rarity: Rarities.Common });
       assert.equal(item.atk(), atk);
     });
   });
